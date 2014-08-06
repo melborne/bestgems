a77fd6a011b087f7b3447fcb8640eaddd341f954
kyoendo <postagie@gmail.com>
2014-08-07 08:45:18 +0900

    define Bestgems endpoint

--- lib/bestgems.rb
+++ lib/bestgems.rb
@@ -1,5 +1,21 @@
 require "bestgems/version"
+require "bestgems/client"
 
 module Bestgems
-  # Your code goes here...
+  API_HOST = "http://bestgems.org/api".freeze
+  API_VERSION  = "v1".freeze
+
+  class << self
+    def api_host
+      ENV['BESTGEMS_API_HOST'] || API_HOST
+    end
+
+    def api_version
+      ENV['BESTGEMS_API_VERSION'] || API_VERSION
+    end
+
+    def api_gems_endpoint
+      File.join(api_host, api_version, 'gems')
+    end
+  end
 end
